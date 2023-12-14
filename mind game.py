85ee2656f02f8796c13bf9bbbfe1b28a0b95ceb6
monish <153736074+Monishrepository@users.noreply.github.com>
2023-12-14 20:53:41 +0530

    Add files via upload

---
+++ mind game.py
@@ -0,0 +1,25 @@
+import random
+ran=random.randint(1,1000)
+ans=ran/2
+print("Everything Keep It Mind")
+print("""say Y/N  "y=yes and N-quit" """)
+a=input("Imagine One Specific Amount\n").upper()
+if a=="Y":
+    b=input("Credit The Same Amount And Add It\n").upper()
+    if b=="Y":
+        print("I Give You",ran,"$ Add It\n")
+        c=input("").upper()
+        if c=="Y":
+            d=input("Half Of The Amount Give It To Charity\n").upper()
+            if d=="Y":
+                e=input("Return The Credit\n").upper()
+                if e=="Y":
+                    print("You Have",ans,"$ Am I Right?")
+print("Game Over")
+
+                    
+                
+                
+            
+        
+    
